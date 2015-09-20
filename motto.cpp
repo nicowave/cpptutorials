ccec1df8c54c761f7ceec5f4c14552c351652f2d
nicowave <geoidlife@gmail.com>
2015-09-19 23:07:02 -0700

    first commit of some very basic C++ tutorials

---
+++ motto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	std::cout << "Solidum petit in profundis!\n";
+	std::cout << "Hello world!\n";
+	std::cout << "I am a C++ program, \n";
+	std::cout << "I need to be compiled and linked before run.\n";
+	std::cout << "Aint that some fun?!\n";	
+	std::cout << "I already miss Swift\n";
+	cout << "semi-colons are are more than semi-lame\n";
+	return 0;
+}
